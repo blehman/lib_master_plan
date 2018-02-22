9cd2326a3261fefec60c9a414d3f75fbad996af0
Brian Lehman <brian@viz.farm>
2018-02-22 02:20:48 -0700

    functional resize - need to update the text

--- js/app.js
+++ js/app.js
@@ -24,38 +24,60 @@
       , "total_lookup":total_lookup
     }]
 
-    var container = d3.select(".viz-container");
+    // draw initial viz
+    reDraw();
+    var run;
+    window.onresize = function() {
+        clearTimeout(run);
+        run = setTimeout(function() {
+            reDraw();
+        }, 100);
+    };
 
-    var viz_width = container.node().getBoundingClientRect().width
-      , viz_height = container.node().getBoundingClientRect().height
-      , k = (viz_width/687.96875*1.10) // ideal ratio
-      , left_margin = (k>=1)?60:60*k;
 
-    // set measurements
-    d3.select("#viz")
-      .attr("preserveAspectRatio","xMinYMin meet")
-      .attr("viewBox","-60 -12 " + (viz_width*1.10+60)+ " " +(viz_height*1.15+12))
-    console.log(viz_width)
-    console.log(viz_height)
+    function reDraw(){
+      var container = d3.select(".viz-container");
+      var width = container.node().getBoundingClientRect().width
+        , height = container.node().getBoundingClientRect().height
+        , margin = {top: 20, right: 10, bottom: 30, left: 30}
+        , viz_width = width - margin.left - margin.right
+        , viz_height = height - margin.top - margin.bottom
 
-    iBarChart.width(viz_width)
-    iBarChart.height(viz_height)
-    iBubbles.width(viz_width)
-    iBubbles.height(viz_height)
-    container.select("svg").selectAll("#barChart-container")
-      .data(full_set)
-      .enter()
-      .append("g")
-      .attr('id',"barChart-container")
-      .call(iBarChart);
-    container.select("svg").selectAll("#bubbles-container")
-      .data(full_set)
-      .enter()
-      .append("g")
-      .attr('id',"bubbles-container")
-      .call(iBubbles);
-    window.addEventListener("resize", function(){
-      //wtf
-    });
+        //, k = (viz_width/687.96875*1.10) // ideal ratio
+        //, left_margin = (k>=1)?60:60*k;
+        //console.log(viz_width)
+        //console.log(viz_height)
+        //d3.select("#viz")
+          //.attr("preserveAspectRatio","xMinYMin meet")
+          //.attr("viewBox","-60 -12 " + (viz_width*1.10+60)+ " " +(viz_height*1.15+12))
+      iBarChart.width(viz_width)
+      iBarChart.height(viz_height)
+      iBubbles.width(viz_width)
+      iBubbles.height(viz_height)
+      // remove all children in svg
+      container.select("svg").selectAll("*").remove()
+      // build margins on svg & add svg-elments-container
+      container.select("svg")
+        .attr("width", viz_width + margin.left + margin.right)
+        .attr("height", viz_height + margin.top + margin.bottom)
+        .append("g")
+        .attr("id","svg-elements-container")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // draw barChart
+      d3.select("#svg-elements-container").selectAll("#barChart-container")
+        .data(full_set)
+        .enter()
+        .append("g")
+        .attr('id',"barChart-container")
+        .call(iBarChart);
+      // draw bubbles
+      d3.select("#svg-elements-container").selectAll("#bubbles-container")
+        .data(full_set)
+        .enter()
+        .append("g")
+        .attr('id',"bubbles-container")
+        .call(iBubbles);
+    }
   }
 }())
